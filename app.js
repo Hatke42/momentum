859fb3e8434abbd02c05736c655c6f85f7bc4d4b
Hatke42 <nayanchaudhary42@gmail.com>
2023-03-03 19:21:55 +0530

    Storing and retriving name working

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const greet = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
 function onLoginBtnClick(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  storeUserName(username);
+  greetUser(username);
 }
 
-loginForm.addEventListener("submit", onLoginBtnClick);
+function greetUser(username) {
+  greet.classList.remove(HIDDEN_CLASSNAME);
+  greet.innerText = `Hello ${username}`;
+}
+
+function storeUserName(username) {
+  localStorage.setItem(USERNAME_KEY, username);
+}
+
+if (savedUsername !== null) {
+  greetUser(savedUsername);
+} else {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginBtnClick);
+}
