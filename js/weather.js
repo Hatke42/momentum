a59d9b5d6373b6b6db719cd5e61420ae0adcb564
Nayan Chaudhary <nayanchaudhary42@gmail.com>
2023-03-22 23:13:24 +0530

    removing API KEY from file

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,9 @@
-const API_KEY = "bbf7e59842f4b7f18dad8b2efabaf1cb";
-
 function success(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://handle.hatke.workers.dev/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
+  console.log(url);
   fetch(url)
     .then((data) => data.json())
     .then((data) => {
